fbf7b8a3a635bf6400b29cc18d67e7ff412831e2
vic <vaitt_joy@163.com>
2018-02-23 11:04:17 +0800

    添加 model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=30, verbose_name='菜单名称')
+    hrefLink = models.CharField(verbose_name='跳转链接')
+    allow = models.IntegerField(default=-1, verbose_name='权限')
+    sort = models.IntegerField(verbose_name='排序')
+    parentId = models.IntegerField(verbose_name='父节点id')
